dd1c460740ef22d36307e17d964607add8f45b84
Raymond Laptop <raymdelta@gmail.com>
2020-02-04 23:34:33 +0100

    project start, mesh, cube, rounded cube, cube sphere & mesh deformation

---
+++ GPE ex lesson 1/Assets/Grid.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    [SerializeField]
+    int xSize;
+    [SerializeField]
+    int ySize;
+
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    private void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+        Generate();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void Generate()
+    {
+        //Generate each gizmo with a delay (coroutined)
+        WaitForSeconds wait = new WaitForSeconds(0.04f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        //Generate vector filled with grid dots for the 
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        //Create UV coordinates for the grid
+        Vector2[] uv = new Vector2[vertices.Length];
+        //For all dots on the origin move the x and y so they dont overlap
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for(int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+
+        int[] tris = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                tris[ti + 0] = vi;
+                tris[ti + 3] = tris[ti + 2] = vi + 1;
+                tris[ti + 4] = tris[ti + 1] = vi + xSize + 1;
+                tris[ti + 5] = vi + xSize + 2;
+                mesh.triangles = tris;
+            }
+        }
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Only draw when there are vertices (play mode)
+        if(vertices == null)
+        {
+            return;
+        }
+
+        //Create gizmo for each vertice
+        Gizmos.color = Color.black;
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
---
+++ GPE ex lesson 1/Assets/MeshDeformer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour
+{
+
+    Mesh deformingMesh;
+    //Keep track of the original vertice positions
+    Vector3[] originalVertices, displacedVertices;
+    Vector3[] vertexVelocities;
+
+    public float springForce = 10f;
+    public float damping = 5f;
+    float uniformScale = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        //Save original vertices
+        originalVertices = deformingMesh.vertices;
+        displacedVertices = new Vector3[originalVertices.Length];
+        for (int i = 0; i < originalVertices.Length; i++)
+        {
+            displacedVertices[i] = originalVertices[i];
+        }
+        //Vector to save the velocity of each vertex
+        vertexVelocities = new Vector3[originalVertices.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uniformScale = transform.localScale.x;
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            UpdateVertex(i);
+        }
+        deformingMesh.vertices = displacedVertices;
+        deformingMesh.RecalculateNormals();
+    }
+
+    void UpdateVertex(int i)
+    {
+        Vector3 velocity = vertexVelocities[i];
+        Vector3 displacement = displacedVertices[i] - originalVertices[i];
+        displacement *= uniformScale;
+        velocity -= displacement * springForce * Time.deltaTime;
+        velocity *= 1f - damping * Time.deltaTime;
+        vertexVelocities[i] = velocity;
+        displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
+    }
+
+    public void AddDeformingForce(Vector3 point, float force)
+    {
+        point = transform.InverseTransformPoint(point);
+        Debug.DrawLine(Camera.main.transform.position, point);
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            AddForceToVertex(i, point, force);
+        }
+    }
+
+    void AddForceToVertex(int i, Vector3 point, float force)
+    {
+        Vector3 pointToVertex = displacedVertices[i] - point;
+        pointToVertex *= uniformScale;
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.deltaTime;
+        vertexVelocities[i] += pointToVertex.normalized * velocity;
+    }
+}
